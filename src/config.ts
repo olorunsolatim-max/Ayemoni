6494e9b94d39c10b2e8e9015dd4cb11c72b941a1
olorunsolatim-max <olorunsolatim@gmail.com>
2026-04-15 19:29:05 +0100

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,9 +1,9 @@
 export const SITE = {
-  website: "https://astro-paper.pages.dev/", // replace this with your deployed domain
-  author: "Sat Naing",
-  profile: "https://satnaing.dev/",
+  website: "https://ayemoni.vercel.app/", // replace this with your deployed domain
+  author: "Ayemoni",
+  profile: "https://www.facebook.com/profile.php?id=100086089816758",
   desc: "A minimal, responsive and SEO-friendly Astro blog theme.",
-  title: "AstroPaper",
+  title: "FBC News",
   ogImage: "astropaper-og.jpg",
   lightAndDarkMode: true,
   postPerIndex: 4,
